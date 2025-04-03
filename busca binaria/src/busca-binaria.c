fbdff4d87231749e20d6fcc33295527ba70dc9b0
vinicius Leoni <viniciuslg@dcc.ufrj.br>
2025-04-03 10:25:47 -0300

    Update busca-binaria.c

--- busca binaria/src/busca-binaria.c
+++ busca binaria/src/busca-binaria.c
@@ -11,15 +11,17 @@ Função que realiza busca binária em um vetor ordenado
 */
 int buscaBinaria(int vet[], int esq, int dir, int valor) {
     int tam = dir-esq+1;
-    int meio = (dir+esq)/2;
+
     esq = 0;
     dir = tam - 1;
     while(esq <= dir) {
-    meio = (esq + dir)/2;
+    int meio = (esq + dir)/2;
     if(vet[meio] == valor)
     return meio;
-    if(valor > vet[meio]) esq = meio + 1;
-    else dir = meio - 1;
+    if(valor > vet[meio]) 
+        esq = meio + 1;
+    else 
+        dir = meio - 1;
     }
     return -1;//valor fora dos limites do vetor ou até fora do vetor todo
     }
